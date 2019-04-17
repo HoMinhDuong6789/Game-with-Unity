cef188fcd1e04bce144d1020fa2026b78c30f029
Ho Minh Duong <minhans29@gmail.com>
2019-04-17 23:53:49 +0700

    video 8 9

--- ProYoutube/Assets/Script/Player.cs
+++ ProYoutube/Assets/Script/Player.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Player : MonoBehaviour
+public class Player:MonoBehaviour
 {
 
     public float speed = 50f, maxspeed = 3, jumpPow = 220f;
     public bool grounded = true, faceright = true, doublejump = false;
+
+    public int ourHealth;     // so luong mau hien tai
+    public int maxhealth = 5;       // so luong mau toi da
 
     public Rigidbody2D r2;
     public Animator anim;
@@ -16,6 +20,7 @@ public class Player : MonoBehaviour
     {
         r2 = gameObject.GetComponent<Rigidbody2D>();
         anim = gameObject.GetComponent<Animator>();
+        ourHealth = maxhealth;          // qua moi level so luong mau duoc reset lai
     }
 
     // Update is called once per frame
@@ -33,6 +38,7 @@ public class Player : MonoBehaviour
                 r2.AddForce(Vector2.up * jumpPow);
 
             }
+
             else
             {
                 if (doublejump)
@@ -40,8 +46,12 @@ public class Player : MonoBehaviour
                     doublejump = false;
                     r2.velocity = new Vector2(r2.velocity.x, 0);
                     r2.AddForce(Vector2.up * jumpPow * 0.7f);
+
                 }
             }
+
+
+
 
         }
     }
@@ -69,9 +79,12 @@ public class Player : MonoBehaviour
         if (grounded)
         {
             r2.velocity = new Vector2(r2.velocity.x * 0.7f, r2.velocity.y);
-
         }
 
+        if (ourHealth <= 0)
+        {
+            Death();
+        }
     }
 
     public void Flip()
@@ -81,5 +94,24 @@ public class Player : MonoBehaviour
         Scale = transform.localScale;
         Scale.x *= -1;
         transform.localScale = Scale;
+    }
+
+    public void Death()
+    {
+        //dung de set lai mang level cua nguoi choi luc chet
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Damage(int damage)
+    {
+        ourHealth -= damage;
+        gameObject.GetComponent<Animation>().Play("redflash");
+    }
+    // bi mat mau khi dung phai hang gai
+    public void Knockback(float Knockpow, Vector2 Knockdir)
+    {
+        r2.velocity = new Vector2(0, 0);
+        r2.AddForce(new Vector2(Knockdir.x * -100, Knockdir.y * Knockpow));
     }
 }
